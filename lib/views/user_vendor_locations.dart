ab0e2806692bb63f0522da7eef030b7695f52022
talhadiscover <106244350+talhadiscover@users.noreply.github.com>
2022-06-05 16:47:16 +0500

    latest

--- lib/views/user_vendor_locations.dart
+++ lib/views/user_vendor_locations.dart
@@ -8,111 +8,121 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class uservendoes_location extends StatefulWidget {
   const uservendoes_location({Key? key}) : super(key: key);
 
-
-
   @override
   State<uservendoes_location> createState() => _uservendoes_locationState();
 }
 
 class _uservendoes_locationState extends State<uservendoes_location> {
-  Set<Marker> _markers = <Marker>{};
+  GoogleMapController? mapController; //contrller for Google map
+  Set<Marker> markers = Set(); //markers for google map
 
-  // declare a global key
-  final GlobalKey globalKey = GlobalKey();
+  LatLng startLocation = LatLng(27.6602292, 85.308027);
+  LatLng endLocation = LatLng(27.6599592, 85.3102498);
+  LatLng carLocation = LatLng(27.659470, 85.3077363);
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          body: Stack(
-            children: [
+  void initState() {
+    addMarkers();
+    super.initState();
+  }
 
-              // you have to add your widget in the same widget tree
-              // add your google map in stack
-              // declare your marker before google map
-              // pass your global key to your widget
+  addMarkers() async {
+    BitmapDescriptor markerbitmap = await BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(),
+      "images/iconbike.png",
 
-              MyMarker(globalKey),
+    );
 
-              Positioned.fill(
-                child: GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                      target: LatLng(32.4279, 53.6880), zoom: 15),
-                  markers: _markers,
-                ),
-              ),
-            ],
+    markers.add(
+        Marker( //add start location marker
+          markerId: MarkerId(startLocation.toString()),
+          position: startLocation, //position of marker
+          infoWindow: InfoWindow( //popup info
+            title: 'Starting Point ',
+            snippet: 'Start Marker',
           ),
-          floatingActionButton: FloatingActionButton.extended(
-            label: FittedBox(child: Text('Add Markers')),
-            onPressed: () async {
+          icon: markerbitmap, //Icon for Marker
+        )
+    );
 
-              // call widgetToIcon Function and pass the same global key
-
-              _markers.add(
-                Marker(
-                  markerId: MarkerId('circleCanvasWithText'),
-                  icon: await MarkerIcon.widgetToIcon(globalKey),
-                  position: LatLng(35.8400, 50.9391),
-                ),
-              );
-              setState(() {});
-            },
+    markers.add(
+        Marker( //add start location marker
+          markerId: MarkerId(endLocation.toString()),
+          position: endLocation, //position of marker
+          rotation:-45,
+          infoWindow: InfoWindow( //popup info
+            title: 'End Point ',
+            snippet: 'End Marker',
           ),
-          floatingActionButtonLocation:
-          FloatingActionButtonLocation.centerFloat,
+          icon: markerbitmap, //Icon for Marker
         ),
-      ],
+
+
     );
-  }
-}
-
-class MyMarker extends StatelessWidget {
-  // declare a global key and get it trough Constructor
-
-  MyMarker(this.globalKeyMyWidget);
-  final GlobalKey globalKeyMyWidget;
-
-  @override
-  Widget build(BuildContext context) {
-    // wrap your widget with RepaintBoundary and
-    // pass your global key to RepaintBoundary
-    return RepaintBoundary(
-      key: globalKeyMyWidget,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            width: 250,
-            height: 180,
-            decoration:
-            BoxDecoration(color: Colors.black, shape: BoxShape.circle),
-          ),
-          Container(
-              width: 220,
-              height: 150,
-              decoration:
-              BoxDecoration(color: Colors.amber, shape: BoxShape.circle),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.accessibility,
-                    color: Colors.white,
-                    size: 35,
-                  ),
-                  Text(
-                    'Widget',
-                    style: TextStyle(color: Colors.white, fontSize: 25),
-                  ),
-                ],
-              )),
-        ],
+    markers.add(
+      Marker( //add start location marker
+        markerId: MarkerId(endLocation.toString()),
+        position: endLocation, //position of marker
+        rotation:-45,
+        infoWindow: InfoWindow( //popup info
+          title: 'End Point ',
+          snippet: 'End Marker',
+        ),
+        icon: markerbitmap, //Icon for Marker
       ),
+
+
+    );
+
+    // String imgurl = "https://www.fluttercampus.com/img/car.png";
+    // Uint8List bytes = (await NetworkAssetBundle(Uri.parse(imgurl))
+    //     .load(imgurl))
+    //     .buffer
+    //     .asUint8List();
+    //
+    // markers.add(
+    //     Marker( //add start location marker
+    //       markerId: MarkerId(carLocation.toString()),
+    //       position: carLocation, //position of marker
+    //       infoWindow: InfoWindow( //popup info
+    //         title: 'Car Point ',
+    //         snippet: 'Car Marker',
+    //       ),
+    //       icon: BitmapDescriptor.fromBytes(bytes), //Icon for Marker
+    //     )
+    // );
+    //
+    // setState(() {
+    //   //refresh UI
+    // });
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        appBar: AppBar(
+          title: Text("Image Marker on Google Map"),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: GoogleMap( //Map widget from google_maps_flutter package
+          zoomGesturesEnabled: true, //enable Zoom in, out on map
+          initialCameraPosition: CameraPosition( //innital position in map
+            target: startLocation, //initial position
+            zoom: 14.0, //initial zoom level
+          ),
+          markers: markers, //markers to show on map
+          mapType: MapType.normal, //map type
+          onMapCreated: (controller) { //method called when map is created
+            setState(() {
+              mapController = controller;
+            });
+          },
+        )
     );
   }
 }
+
+
 
 
 
